2fd1a764a7daeea1f37356e634e592f9a1c950b4
mhenke10 <mhenke10@gmail.com>
2015-04-13 18:00:56 -0500

    Initial Commit

---
+++ MicroPay News/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MicroPay News
+//
+//  Created by Michael Henke on 4/13/15.
+//  Copyright (c) 2015 Group 1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
