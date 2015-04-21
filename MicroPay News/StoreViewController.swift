1fee703d92d5990404c723f535cb6b558b20957a
Julia Duncan <jad667@mail.missouri.edu>
2015-04-21 17:47:24 -0500

    Added Store view
    Store view and description

---
+++ MicroPay News/StoreViewController.swift
@@ -0,0 +1,26 @@
+//
+//  StoreViewController.swift
+//  MicroPay News
+//
+//  Created by Julia Duncan on 4/21/15.
+//  Copyright (c) 2015 Group 1. All rights reserved.
+//
+
+import UIKit
+
+class StoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        var navigationBarAppearace = UINavigationBar.appearance()
+        self.title = "Store"
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
